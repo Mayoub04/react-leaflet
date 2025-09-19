c84a1341f295b4ce1e83485d6724ba50159f9411
mbenazza <m.benazza@lprs.fr>
2025-09-19 17:48:55 +0200

    Ajout des marqueurs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import './App.css';
 import "leaflet/dist/leaflet.css";
-import {MapContainer, TileLayer} from "react-leaflet"
+
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import MarkerClusterGroup from "react-leaflet-markercluster";
+
+import { Icon, divIcon, point } from "leaflet";
+
+const customIcon = new Icon({
+  // iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+  iconUrl: require("./img/marker-icon.png"),
+  iconSize: [38, 38]
+});
+
+const markers = [
+  {
+    geocode: [48.86, 2.3522],
+    popUp: "Hello, I am pop up 1"
+  },
+  {
+    geocode: [48.85, 2.3522],
+    popUp: "Hello, I am pop up 2"
+  },
+  {
+    geocode: [48.855, 2.34],
+    popUp: "Hello, I am pop up 3"
+  }
+];
 
 
 function App() {
@@ -10,8 +35,14 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
+      
+       {markers.map((marker) => (
+          <Marker position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}      
     </MapContainer>
+    
   );
 }
 
